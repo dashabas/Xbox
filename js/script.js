34c588819fa2f3a226269b7631a14654f4c2d965
dashabasya <dashabasya29@gmail.com>
2022-01-04 14:18:51 +0100

    add slide-one

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    $('.owl-carousel').owlCarousel({
+    $('.slide-one').owlCarousel({
         loop:true,
         margin:0,
         nav:true,
@@ -16,4 +16,23 @@ $(document).ready(function(){
             }
         }
     });
+
+    $('.slide-two').owlCarousel({
+        loop:true,
+        margin:16,
+        nav:true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+
+
+            1000:{
+                items:3.75
+            }
+        }
+    });
 });
+
